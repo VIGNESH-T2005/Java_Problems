e788d5a3f14a00be59860ec26dc8aabdac508a95
VIGNESH T <vigneshhari8739@gmail.com>
2026-09-04 21:40:32 +0530

    005_Swap_Num

---
+++ src/Problems/_006_Swap_Num.java
@@ -0,0 +1,17 @@
+package Problems;
+import java.util.*;
+public class _006_Swap_Num {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		a=a^b;//a=a+b;
+		b=a^b;//b=a-b;
+		a=a^b;//a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
