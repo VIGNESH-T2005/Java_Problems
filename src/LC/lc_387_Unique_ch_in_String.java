e5e5f08074a30ef2f31aa326d1f9bb0ac56c3479
VIGNESH T <vigneshhari8739@gmail.com>
2026-09-05 13:53:20 +0530

    Lc_387_Unique_ch_in_String

---
+++ src/LC/lc_387_Unique_ch_in_String.java
@@ -0,0 +1,21 @@
+package LC;
+import java.util.*;
+public class lc_387_Unique_ch_in_String {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int [] a=new int[26];
+		for(int i=0;i<s.length();i++) {
+			a[s.charAt(i)-'a']++;
+		}
+		for(int i=0;i<s.length();i++) {
+			if(a[s.charAt(i)-'a']==1) {
+				System.out.println(i);
+				break;
+			}
+		}
+
+	}
+
+}
