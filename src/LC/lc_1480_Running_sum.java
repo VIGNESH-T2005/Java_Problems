2d4fa2c29403ae3a9d7d76026ae0811fdaab4641
VIGNESH T <vigneshhari8739@gmail.com>
2026-09-05 13:20:30 +0530

    Lc_1480_Running_Sum

--- src/LC/lc_1480_Running_sum.java
+++ src/LC/lc_1480_Running_sum.java
@@ -1,9 +1,19 @@
 package LC;
-
+import java.util.*;
 public class lc_1480_Running_sum {
 
 	public static void main(String[] args) {
-		
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		int [] a=new int [size];
+		for(int i=0;i<size;i++) {
+			a[i]=sc.nextInt();
+		}
+		int sum=0;
+		for(int i=0;i<size;i++) {
+			sum+=a[i];
+			a[i]=sum;
+		}
+		System.out.println(Arrays.toString(a));
 	}
-
 }
