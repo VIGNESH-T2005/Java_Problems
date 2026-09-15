5fb5588fcec4f70c61b18459ab81ac866d5faa96
VIGNESH T <vigneshhari8739@gmail.com>
2026-09-15 22:47:50 +0530

    product array

--- src/Problems/_017_Product_Array.java
+++ src/Problems/_017_Product_Array.java
@@ -8,7 +8,24 @@ public class _017_Product_Array {
 		for(int i=0;i<size;i++) {
 			arr[i]=sc.nextInt();
 		}
+		int[] left=new int[size];
+		int[] right=new int[size];
+		int[] product=new int[size];
+		left[0]=1;
+		for(int i=1;i<size;i++) {
+			left[i]=left[i-1]*arr[i-1];
+		}
+		right[size-1]=1;
+		for(int i=size-2;i>=0;i--) {
+			right[i]=right[i+1]*arr[i+1];
+		}
+		for(int i=0;i<size;i++) {
+			product[i]=left[i]*right[i];
+		}
+		for(int n: product) {
+			System.out.println(n);
+		}
 		}
 	}
 
-}
+
