ab0f9dca5ca843d6dafdc4179358d62e2e326a8e
VIGNESH T <vigneshhari8739@gmail.com>
2026-09-16 21:54:58 +0530

    18_Max_Min

---
+++ src/Problems/_018_Max_Min.java
@@ -0,0 +1,26 @@
+package Problems;
+import java.util.*;
+public class _018_Max_Min {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr=new int [size];
+		int[] res=new int [size];
+		for(int i=0;i<size;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int left=0,right=size-1,i=0;
+		while(left<=right) {
+				if(i%2==0) {
+				res[i++]=arr[right--];
+				}
+				else {
+				res[i++]=arr[left++];
+				}
+			
+		}
+		System.out.println(Arrays.toString(res));
+	}
+
+}
